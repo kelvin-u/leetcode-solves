043aaf8ec383c1981cb50653c14b4628fe960dd3
Kelvin Yu <yukaiwenn@gmail.com>
2025-06-22 21:33:46 -0400

    new one

--- 3-Sliding Window/2long_substring.py
+++ 3-Sliding Window/2long_substring.py
@@ -1,2 +1,18 @@
+s = "abcabcbb"
+
 def lengthOfLongestSubstring(str):
-    pass
+    longestSet = set()
+    
+    l = 0
+    result = 0
+    
+    for r in range(len(str)):
+        # if in substring
+        while str[r] in longestSet:
+            longestSet.remove(str[l])
+            l += 1
+        longestSet.add(str[r])
+        result = max(result,r-l+1)
+    return result
+
+lengthOfLongestSubstring(s)
