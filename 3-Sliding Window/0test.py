947714370a3cf499309e2979da425013eb614ffe
Kelvin Yu <yukaiwenn@gmail.com>
2025-07-02 14:27:47 -0400

    np

--- 3-Sliding Window/0test.py
+++ 3-Sliding Window/0test.py
@@ -2,14 +2,20 @@ s = "ABABAA"
 k = 2
 
 l = 0 
-length = 0 
+len = 0 
 charSet = {}
 
 for r in range(len(s)):
+    # build the hash map for the values
     charSet[s[r]] = charSet.get(s[r], 0) + 1
+    
+    # window size - highest frequ
 
-    while (r-l+1) - max(charSet.values()) > k:
-        charSet[s[l]] -= 1
-        l += 1
 
-    length = max(length, r - l + 1)
+
+    
+    
+        
+        
+    
+    
--- 3-Sliding Window/3long_repeat.py
+++ 3-Sliding Window/3long_repeat.py
@@ -8,9 +8,12 @@ def characterReplacement(s, k):
     charCount = {}
     
     for r in range(len(s)):
+        # frequency count of the string
         charCount[s[r]] = charCount.get(s[r], 0) + 1
 
+        # while the window size - the highest count char > k we need to adjust the window
         while (r - l + 1) - max(charCount.values()) > k:
+            # remove the frequency from the left then update the pointer
             charCount[s[l]] -= 1
             l += 1
         length = max(length, r - l + 1)
