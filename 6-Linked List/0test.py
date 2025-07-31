2791b119c09a0f5df300cfebd6367ca5e599f8f1
kelvin-u <yukaiwenn@gmail.com>
2025-07-30 21:57:52 -0400

    wrapped up ll

--- 6-Linked List/0test.py
+++ 6-Linked List/0test.py
@@ -1,12 +1,24 @@
-class Animal:
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.Next = None
+
+def merge_lists(l1, l2):
+    dummy = Node()
+    start = dummy
+
+    while l1 and l2:
+        if l1.val < l2.val:
+            start.next = l1
+            l1 = l1.next
+        else:
+            start.next = l2
+            l2 = l2.next
     
-    def __init__(self, name):
-        self.name = name
-        
-    def __repr__(self):
-        return f'Anime name: {self.name}'
+    # not edge case where != l1 = [1,2,3] l2 = [], remaining people bro attach the rest to the tail 
+    if l1:
+        start.next = l1
+    elif l2:
+        start.next = l2
 
-
-if __name__ == '__main__':
-    cat1 = Animal('lucy')
-    print(repr(cat1))
+    return dummy.next
