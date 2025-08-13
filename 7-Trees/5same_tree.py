16274f93e48666b98df4c4acb573c160ba37e59e
Kelvin Yu <yukaiwenn@gmail.com>
2025-08-12 20:40:37 -0400

    new problem

--- 7-Trees/5same_tree.py
+++ 7-Trees/5same_tree.py
@@ -6,4 +6,10 @@ class TreeNode:
 
 
 def isSameTree(p, q):
-    pass
+    if p is None and q is None:
+        return True
+    if p is None or q is None:
+        return False
+    if p.val != q.val:
+        return False
+    return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
